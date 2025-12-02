8adc0f648a86252dce1daae1cc49021eff5f4bb5
JuanCarlos.Dorado <jucagolddev@gmail.com>
2025-12-02 12:46:48 +0100

    Front Estructure

---
+++ frontend/src/app/features/projects/project-detail/project-detail.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project-detail.component.html',
+  styleUrls: ['./project-detail.component.scss']
+})
+export class ProjectDetailComponent {
+
+}
---
+++ frontend/src/app/features/projects/project-upload/project-upload.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-project-upload',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './project-upload.component.html',
+  styleUrls: ['./project-upload.component.scss']
+})
+export class ProjectUploadComponent {
+
+}
