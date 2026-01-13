7ef0567dad6a7d1e58180d94d63e2d203ea5d058
JuanCarlos.Dorado <jucagolddev@gmail.com>
2026-01-13 11:16:26 +0100

    Html 1.1

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HeaderComponent } from './layout/main-layout/header/header.component';
+import { FooterComponent } from './layout/main-layout/footer/footer.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HeaderComponent, FooterComponent],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
